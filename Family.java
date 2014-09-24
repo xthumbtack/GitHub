3a126c938063f329815c11289e62b5747260640b
xthumbtack <ry.somphithack@gmail.com>
2014-09-24 08:04:39 -0700

    Family.java
    ryanh

---
+++ Family.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+public class Family {
+    
+    int Income = 0;
+    int children = 0;
+    static int randomInt = 0;
+    
+    static {  
+        Random rdm = new Random();
+        int randomInt = rdm.nextInt(7) + 1;
+    }
+    
+    
+    {
+        children = randomInt;
+    
+    }
+    Family() {
+        
+        Income = randomInt * 1000;
+    }
+    
+    Family(int num) {
+        String userInput = "";
+        double numDouble = 0.0;
+        userInput = JOptionPane.showInputDialog("\n Input your income : ");
+        
+        Double varDouble = new Double(userInput);
+        numDouble = varDouble.doubleValue();
+        
+        System.out.println("Double, " + numDouble + varDouble);
+    }
+    
+    public static void main (String []args) {
+        
+     
+        if (randomInt < 4) { 
+            Family obj1 = new Family();
+        }
+        else {
+            Family obj2 = new Family(randomInt);
+        }
+    }
+        
+}
+
